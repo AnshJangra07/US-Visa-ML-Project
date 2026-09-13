645e760ca2a153a0a2e0b48fbcb41031a80bf3dc
Ansh Jangra <anshjangra1234@gmail.com>
2026-09-13 18:48:56 +0530

    update constants gitignore

---
+++ us_visa/constants/__init__.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+from datetime import date
+load_dotenv()
+MongoDB_URL = os.getenv("MONGODB_URL")
+
+
+DATABASE_NAME = "US_VISA"
+
+COLLECTION_NAME = "VISA_DATA"
+
+MONGODB_URL_KEY = MongoDB_URL
+
+PIPELINE_NAME : str = "usvisa"
+ARTIFACT_DIR : str = "artifact"
+
+MODEL_FILE_NAME = "model.pkl"
+
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_COLLECTION_NAME: str = "visa_data"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
