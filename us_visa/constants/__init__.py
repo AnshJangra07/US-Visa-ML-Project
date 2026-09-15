cf96642881f70e794675a27ab1b7131d27e01edb
Ansh Jangra <anshjangra1234@gmail.com>
2026-09-16 01:06:27 +0530

    model trainer added

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -3,7 +3,7 @@ from datetime import date
 
 DATABASE_NAME = "US_VISA"
 
-COLLECTION_NAME = "visa_data"
+COLLECTION_NAME = "VISA_DATA"
 
 MONGODB_URL_KEY = "MONGODB_URL"
 
@@ -34,7 +34,7 @@ SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
-DATA_INGESTION_COLLECTION_NAME: str = "visa_data"
+DATA_INGESTION_COLLECTION_NAME: str = "VISA_DATA"
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
@@ -59,14 +59,14 @@ DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
 
 
-# """
-# MODEL TRAINER related constant start with MODEL_TRAINER var name
-# """
-# MODEL_TRAINER_DIR_NAME: str = "model_trainer"
-# MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
-# MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
-# MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
-# MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
+"""
+MODEL TRAINER related constant start with MODEL_TRAINER var name
+"""
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.8
+MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
 
 
 # """
